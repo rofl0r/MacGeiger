a952c516707990df0f6411fb42b52de9ac823552
rofl0r <retnyg@gmx.net>
2016-07-11 02:50:11 +0100

    factor out header stuff from channel-switch.c

--- channel-switch.c
+++ channel-switch.c
@@ -4,64 +4,7 @@
 #include <net/if.h>
 #include <stropts.h>
 #include <unistd.h>
-
-#define SIOCSIWFREQ 0x8B04
-#define IW_FREQ_FIXED 0x01
-
-struct iw_quality {
-	unsigned char qual;
-	unsigned char level;
-	unsigned char noise;
-	unsigned char updated;
-};
-
-struct iw_param {
-	int value;
-	unsigned char fixed;
-	unsigned char disabled;
-	unsigned short flags;
-};
-
-struct iw_point {
-	void *pointer;
-	unsigned short length;
-	unsigned short flags;
-};
-
-struct iw_freq {
-	int m;
-	short e;
-	unsigned char i;
-	unsigned char flags;
-};
-
-union iwreq_data {
-	char name[IFNAMSIZ];
-	struct iw_point	essid;
-	struct iw_param	nwid;
-	struct iw_freq	freq;
-	struct iw_param	sens;
-	struct iw_param	bitrate;
-	struct iw_param	txpower;
-	struct iw_param	rts;
-	struct iw_param	frag;
-	unsigned mode;
-	struct iw_param	retry;
-	struct iw_point	encoding;
-	struct iw_param	power;
-	struct iw_quality qual;
-	struct sockaddr	ap_addr;
-	struct sockaddr	addr;
-	struct iw_param	param;
-	struct iw_point	data;
-};
-
-struct	iwreq {
-	union {
-		char ifrn_name[IFNAMSIZ];
-	} ifr_ifrn;
-	union iwreq_data u;
-};
+#include "wireless-lite.h"
 
 /* returns 0 on success. */
 int set_channel(const char* iface, int channel) {
